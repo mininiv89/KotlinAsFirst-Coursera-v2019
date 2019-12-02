97cb8850f1a487db19e93e8dfd2a4987ee20328c
Igor <mininiv89@yandex.ru>
2019-12-02 17:54:24 +0300

    Выполнена lesson3 задача на число фибаначи (сделано путем цикла и рекурсии)

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -75,7 +75,20 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+//    return if (n <= 2) 1
+//    else fib(n - 2) + fib(n - 1)
+    if (n <= 2) return 1
+    var first = 1
+    var second = 1
+    var result = 0
+    for (i in 3..n) {
+        result = first + second
+        second = first
+        first = result
+    }
+    return result
+}
 
 /**
  * Простая
